b67cc40062a7db037173d45d17c258858686f232
jparkley <parkleydondon@yahoo.com>
2021-11-05 20:53:51 -0700

    Completed simple To Do

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,17 +1,64 @@
-
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState({
+    toDo: '',
+    toDos: [] /* {name: String, completed: false} */
+  })
+
+  const handleSubmit = (e) => {    
+    e.preventDefault()
+    if (!state.toDo) return
+
+    setState(prev => {
+      return {
+        toDos: [...prev.toDos, {name: state.toDo, completed: false}],
+        toDo: ''
+      }
+    })
+  }
+
+  const toggleCompleted = (index) => {
+    setState(prev => {
+      let updatedToDos = prev.toDos.map((toDo, i) => {
+        if(i === index) { return {...toDo, completed: !toDo.completed}}
+        return toDo
+      })
+      return {...prev, toDos: updatedToDos}
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div className="container">
+      <div className="container-header">
+        <h2 className="title">To Do List</h2>
+      </div>
+      <div className="container-body">
+        <form action="" onSubmit={handleSubmit}>
+          <input type="text" name="toDo" placeholder="Enter task here ..."
+            value={state.toDo} onChange={e => setState({...state, toDo: e.target.value})}
+          />
+          <button type="button" onClick={handleSubmit}>Add</button>
+        </form>
+        <div className="list-wrapper">
+          <ul>
+            {state.toDos && state.toDos.map((toDo, index) => (
+              <li key={index} onClick={() => toggleCompleted(index)} 
+                className={toDo.completed ? 'completed' : ''}
+              >{capitalize(toDo.name)}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
+}
+
+function capitalize(str) {
+  if (!str) return
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
 export default App;
